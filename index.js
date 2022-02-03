3904c4a0f81897d09efb7ba29e29989fdc816da5
Bevin Duncan <bevin.duncan@gmail.com>
2022-02-02 17:29:07 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, str){
+    const result = drivers.filter(element => element === str || element === str.toLowerCase());
+    return result
+}
+
+function fuzzyMatch(drivers, str){
+    const result = drivers.filter(element => element.startsWith(str));
+    return result;
+}
+
+function matchName(driver, str){
+    const result = driver.filter(element => element.name === str)
+    return result;
+}
